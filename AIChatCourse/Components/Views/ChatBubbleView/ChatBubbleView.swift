be1712bedfeb89b62bb04794840409147bff67e3
jason <15021453094@163.com>
2026-03-22 17:56:00 +0800

    [Feature] Add ChatBubbleView , ChatBubbleViewBuilder

---
+++ AIChatCourse/Components/Views/ChatBubbleView/ChatBubbleView.swift
@@ -0,0 +1,68 @@
+//
+//  ChatBubbleView.swift
+//  AIChatCourse
+//
+//  Created by Elaine on 2026/3/22.
+//
+
+import SwiftUI
+
+struct ChatBubbleView: View {
+    var text: String = "This is sample text."
+    var textColor: Color = .primary
+    var backgroundColor: Color = .init(uiColor: .systemGray6)
+    var imageName: String?
+    var showImage: Bool = true
+    let offset: CGFloat = 14
+    var body: some View {
+        HStack(alignment: .top, spacing: 8) {
+            if showImage {
+                ZStack {
+                    if let imageName {
+                        ImageLoaderView(urlString: imageName)
+                    } else {
+                        Rectangle()
+                            .fill(.secondary)
+                    }
+                }
+                .frame(width: 45, height: 45)
+                .clipShape(Circle())
+                .offset(y: offset)
+            }
+
+            Text(text)
+                .font(.body)
+                .foregroundStyle(textColor)
+                .padding(.vertical, 10)
+                .padding(.horizontal, 16)
+                .background(backgroundColor)
+                .cornerRadius(6)
+        }
+        .padding(.bottom, showImage ? offset : 0)
+    }
+}
+
+#Preview {
+    ScrollView {
+        VStack(spacing: 20) {
+            ChatBubbleView()
+            ChatBubbleView(text: "foregroundStyle foregroundStyle foregroundStyle foregroundStyle foregroundStyle foregroundStyle foregroundStyle foregroundStyle foregroundStyle foregroundStyle foregroundStyle")
+            ChatBubbleView()
+
+            ChatBubbleView(
+                text: "foregroundStyle foregroundStyle foregroundStyle foregroundStyle foregroundStyle foregroundStyle foregroundStyle foregroundStyle foregroundStyle foregroundStyle foregroundStyle",
+                textColor: .white,
+                backgroundColor: .accent,
+                imageName: nil,
+                showImage: false
+            )
+            ChatBubbleView(
+                textColor: .white,
+                backgroundColor: .accent,
+                imageName: nil,
+                showImage: false
+            )
+        }
+        .padding(8)
+    }
+}
---
+++ AIChatCourse/Components/Views/ChatBubbleView/ChatBubbleViewBuilder.swift
@@ -0,0 +1,60 @@
+//
+//  ChatBubbleViewBuilder.swift
+//  AIChatCourse
+//
+//  Created by Elaine on 2026/3/22.
+//
+
+import SwiftUI
+
+struct ChatBubbleViewBuilder: View {
+    var message: ChatMessageModel = .mock
+    var isCurrentUser: Bool = false
+    var imageName: String?
+    var body: some View {
+        ZStack {
+            ChatBubbleView(
+                text: message.content ?? "",
+                textColor: isCurrentUser ? .white : .primary,
+                backgroundColor: isCurrentUser ? .accent : Color(uiColor: .systemGray6),
+                imageName: imageName,
+                showImage: !isCurrentUser
+            )
+            .frame(maxWidth: .infinity, alignment: isCurrentUser ? .trailing : .leading)
+        }
+        .padding(.leading, isCurrentUser ? 75 : 0)
+        .padding(.trailing, isCurrentUser ? 0 : 75)
+    }
+}
+
+#Preview {
+    ScrollView {
+        VStack(spacing: 24) {
+            ChatBubbleViewBuilder()
+            ChatBubbleViewBuilder(isCurrentUser: true)
+            ChatBubbleViewBuilder(
+                message: ChatMessageModel(
+                    id: UUID().uuidString,
+                    chatId: UUID().uuidString,
+                    authorId: UUID().uuidString,
+                    content: "ChatBubbleViewBuilder()ChatBubbleViewBuilder()ChatBubbleViewBuilder()ChatBubbleViewBuilder()ChatBubbleViewBuilder()ChatBubbleViewBuilder()ChatBubbleViewBuilder()ChatBubbleViewBuilder()",
+                    seenByIds: nil,
+                    dateCreated: .now
+                ),
+                isCurrentUser: true
+            )
+            ChatBubbleViewBuilder(
+                message: ChatMessageModel(
+                    id: UUID().uuidString,
+                    chatId: UUID().uuidString,
+                    authorId: UUID().uuidString,
+                    content: "ChatBubbleViewBuilder()ChatBubbleViewBuilder()ChatBubbleViewBuilder()ChatBubbleViewBuilder()ChatBubbleViewBuilder()ChatBubbleViewBuilder()ChatBubbleViewBuilder()ChatBubbleViewBuilder()",
+                    seenByIds: nil,
+                    dateCreated: .now
+                ),
+                isCurrentUser: false
+            )
+        }
+        .padding(12)
+    }
+}
