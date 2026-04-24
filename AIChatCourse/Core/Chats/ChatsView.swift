f437bd42ad9a41ab65c7d9a3f5e37c7e2429bca2
jason <15021453094@163.com>
2026-04-24 09:19:38 +0800

    [Feature] add RecentsAvatars

--- AIChatCourse/Core/Chats/ChatsView.swift
+++ AIChatCourse/Core/Chats/ChatsView.swift
@@ -9,26 +9,15 @@ import SwiftUI
 
 struct ChatsView: View {
     @State private var chats: [ChatModel] = ChatModel.mocks
+    @State private var recentAvatars: [AvatarModel] = AvatarModel.mocks
     @State private var path: [NavigationPathOption] = []
     var body: some View {
         NavigationStack(path: $path) {
-            List(chats, id: \.self) { chat in
-                ChatRowCellViewBuilder(
-                    currentUserId: nil,
-                    chat: chat,
-                    getAvatar: {
-                        try? await Task.sleep(for: .seconds(1))
-                        return AvatarModel.mocks.randomElement()!
-                    },
-                    getLastChatMessage: {
-                        try? await Task.sleep(for: .seconds(1))
-                        return ChatMessageModel.mocks.randomElement()!
-                    }
-                )
-                .anyButton(.highlight, action: {
-                    onChatPressed(chat: chat)
-                })
-                .removeListRowFormatting()
+            List {
+                if !recentAvatars.isEmpty {
+                    recentsSection
+                }
+                chatsSection
             }
             .navigationTitle("Chats")
             .customNavigationDestinationForCoreModule(path: $path)
@@ -40,9 +29,90 @@ struct ChatsView: View {
     ChatsView()
 }
 
+// MARK: 抽取属性
+
+extension ChatsView {
+    private var chatsSection: some View {
+        Section {
+            if chats.isEmpty {
+                Text("Your chats will appear here!")
+                    .font(.title3)
+                    .foregroundStyle(.secondary)
+                    .padding(40)
+                    .frame(maxWidth: .infinity)
+                    .multilineTextAlignment(.center)
+                    .removeListRowFormatting()
+            } else {
+                ForEach(chats) { chat in
+                    ChatRowCellViewBuilder(
+                        currentUserId: nil,
+                        chat: chat,
+                        getAvatar: {
+                            try? await Task.sleep(for: .seconds(1))
+                            return AvatarModel.mocks.randomElement()!
+                        },
+                        getLastChatMessage: {
+                            try? await Task.sleep(for: .seconds(1))
+                            return ChatMessageModel.mocks.randomElement()!
+                        }
+                    )
+                    .anyButton(.highlight, action: {
+                        onChatPressed(chat: chat)
+                    })
+                    .removeListRowFormatting()
+                }
+            }
+        } header: {
+            Text("Chats")
+        }
+    }
+
+    private var recentsSection: some View {
+        Section {
+            ScrollView(.horizontal, showsIndicators: false) {
+                LazyHStack(spacing: 8) {
+                    ForEach(recentAvatars, id: \.self) { avatar in
+                        if let imageName = avatar.profileImageName {
+                            RecentAvatarView(imageName: imageName, title: avatar.name ?? "")
+                                .anyButton {
+                                    onAvatarPressed(avatar: avatar)
+                                }
+                        }
+                    }
+                }
+                .padding(.top, 12)
+            }
+            .frame(height: 120) // 给一个固定的高度
+            .removeListRowFormatting()
+        } header: {
+            Text("Recents")
+        }
+    }
+}
+
+struct RecentAvatarView: View {
+    var imageName: String
+    var title: String
+    var body: some View {
+        VStack {
+            ImageLoaderView(urlString: imageName)
+                .aspectRatio(1, contentMode: .fit)
+                .clipShape(Circle())
+            Text(title)
+                .font(.caption)
+                .foregroundStyle(.secondary)
+        }
+    }
+}
+
 // MARK: 事件
+
 extension ChatsView {
     private func onChatPressed(chat: ChatModel) {
         path.append(.chatView(avatarId: chat.avatarId))
+    }
+    
+    private func onAvatarPressed(avatar: AvatarModel) {
+        path.append(.chatView(avatarId: avatar.avatarId))
     }
 }
