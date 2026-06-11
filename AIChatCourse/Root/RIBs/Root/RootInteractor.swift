47a4848a62623a549d3e51b85ae614012b9b5c78
jason <15021453094@163.com>
2026-06-11 11:22:17 +0800

    [Feature] 整理文件

---
+++ AIChatCourse/Root/RIBs/Root/RootInteractor.swift
@@ -0,0 +1,53 @@
+//
+//  RootInteractor.swift
+//  AIChatCourse
+//
+//  Created by Elaine on 2026/6/11.
+//
+import SwiftUI
+
+@MainActor
+struct RootInteractor {
+    private let authManager: AuthManager
+    private let appState: AppState
+    private let logManager: LogManager
+    private let userManager: UserManager
+    private let purchaseManager: PurchaseManager
+    
+    init(container: DependencyContainer) {
+        self.authManager = container.resolve(AuthManager.self)!
+        self.userManager = container.resolve(UserManager.self)!
+        self.logManager = container.resolve(LogManager.self)!
+        self.purchaseManager = container.resolve(PurchaseManager.self)!
+        self.appState = container.resolve(AppState.self)!
+    }
+    
+    var authUser: UserAuthInfoModel? {
+        authManager.authUser
+    }
+    
+    var showTabBar: Bool {
+        appState.showTabBar
+    }
+    
+    func trackEvent(event: any LoggableEvent) {
+        logManager.trackEvent(event: event)
+    }
+    
+    func trackEvent(eventName: String, parameters: [String: Any]?, type: CustomLogType) {
+        logManager.trackEvent(eventName: eventName, parameters: parameters, type: type)
+    }
+    
+    func login(user: UserAuthInfoModel, isNewUser: Bool) async throws {
+        try await userManager.login(auth: user, isNewUser: isNewUser)
+        try await purchaseManager.logIn(userId: user.uid, attributes: PurchaseProfileAttributes(
+            email: user.email,
+            firebaseAppInstanceID: FirebaseAnalyticsService.appInstanceID,
+            mixpanelDistinctID: MixpanelService.distinctId
+        ))
+    }
+    
+    func signInAnonymously() async throws -> (user: UserAuthInfoModel, isNewUser: Bool) {
+        try await authManager.signInAnonymously()
+    }
+}
---
+++ AIChatCourse/Root/RIBs/Root/RootRouter.swift
@@ -0,0 +1,13 @@
+//
+//  RootRouter.swift
+//  AIChatCourse
+//
+//  Created by Elaine on 2026/6/11.
+//
+import SwiftUI
+
+@MainActor
+struct RootRouter {
+    let router: Router
+    let builder: RootBuilder
+}
